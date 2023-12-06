99682c6f4a88d13eeaa6ba4c99ff0acaa40e5e3f
Clara Miranda Garcia <clara.miranda@galeo.tech>
2023-12-06 20:23:41 +0100

    Adding redirecting and travel list

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, session
 from flask_wtf import FlaskForm
-# from flask_pymongo import PyMongo
-
-from wtforms import StringField, validators, PasswordField, SubmitField, SelectField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
+from wtforms import validators, SubmitField, SelectField
 
 app = Flask(__name__, template_folder='website/resources/templates/', static_folder='website/resources/static/')
-app.secret_key = "secret_key_123"
-# Define database to use (check later)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-# mongo = PyMongo(app)
-
+#app = Flask(__name__)
+app.counter = 0
+app.config['SECRET_KEY'] = '#$%^&*'
 num_people = []
 capitales = [
     ("A Coruña", "g187507"),
@@ -69,11 +66,17 @@ Auxiliar classes and functions for the forms
 
 
 class TravelForm(FlaskForm):
-    destination = SelectField(label='Destino:', validators=[DataRequired()])
-    people = SelectField(label='Número de personas:', choices=[], validators=[DataRequired()])
-    days = SelectField(label='Número de días', choices=[], validators=[DataRequired()])
-    way_transport = SelectField(label='Transporte', choices=[], validators=[DataRequired()])
-    type_trip = SelectField(label='Tipo de viaje', validators=[DataRequired()])
+    choices_dest = []
+    # populating the destination dropdown
+    choices_dest = []
+    for capital in capitales:
+        choices_dest.append(capital[0])
+
+    startdate = DateField('Start Date', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    destination = SelectField(label='Destino:', choices=choices_dest,validators=[DataRequired()])
+    people = SelectField(label='Número de personas:', choices=[(str(i), str(i)) for i in range(1, 20)], validators=[DataRequired()])
+    days = SelectField(label='Número de días', choices=[(str(i), str(i)) for i in range(1, 6)], validators=[DataRequired()])
+    type_trip = SelectField(label='Tipo de viaje', choices = [("cultural", "Cultural"), ("sporty", "Deporte"), ("mix", "Mixto")], validators=[DataRequired()])
     submit = SubmitField("Generar")
 
 
@@ -81,11 +84,9 @@ def populate(travel_form):
     # populating the options of number of people
     travel_form.people.choices = [(str(i), str(i)) for i in range(1, 20)]
     # populating the options of number of days
-    travel_form.days.choices = [(str(i), str(i)) for i in range(1, 11)]
-    # populating the ways of transport
-    travel_form.way_transport.choices = [("car", "En coche"), ("foot", "Senderismo"), ("public", "Transporte público")]
+    travel_form.days.choices = [(str(i), str(i)) for i in range(1, 6)]
     # populating the type of trip
-    travel_form.type_trip.choices = [("cultural", "Cultural"), ("sporty", "Deporte")]
+    travel_form.type_trip.choices = [("cultural", "Cultural"), ("sporty", "Deporte"), ("mix", "Mixto")]
 
     # populating the destination dropdown
     travel_form.destination.choices = []
@@ -97,19 +98,30 @@ def populate(travel_form):
 '''
 Routing of the web
 '''
+
+
 # Creating the main view (Form Contact with the Destination, People, Days, Way of Transport, Type of trip)
 @app.route("/", methods=["GET", "POST"])
-def home():
+def form_route():
     travel_form = TravelForm()
-    populate(travel_form)
+
     # Get the results from the travel_form and process them -> redirect to /travel_list with the resulting data
+    if travel_form.validate_on_submit():
+        people = travel_form.people
+        days = travel_form.days
+        tipo_viaje = travel_form.type_trip
+        destination = travel_form.destination
+        session['startdate'] = travel_form.startdate.data
+        return redirect(url_for('travel_list'))
+
     return render_template("travel_form.html", form=travel_form)
+
 
 @app.route("/travel_list", methods=["GET", "POST"])
 def travel_list():
-    travel_form = TravelForm()
+    startdate = session['startdate']
+    return render_template('travel_list.html')
 
-    return render_template("travel_form.html", form=travel_form)
 
 # decorator for route(argument) function
 @app.route('/admin')
